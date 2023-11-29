8c98725cbc70937d989a461484d5205e07a5c4e4
Chandan <hb7102849@gmail.com>
2023-11-30 02:52:09 +0530

    making Comment section

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -34,7 +34,7 @@ const CommentsList=(({comments})=>{
 const CommentsContainer = () => {
   return (
     <div className='m-5 p-2 '>
-        <h1 className='text-2xl font-bold'> Comments :</h1>
+        <h1 className='text-3xl font-bold'> Comments :</h1>
         <CommentsList comments={CommentData} />
     </div>
   )
