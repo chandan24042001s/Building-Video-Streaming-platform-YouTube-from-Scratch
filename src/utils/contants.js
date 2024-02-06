e557feee984e4c7fe5c7fa9f573055ff92c3cc82
Chandan <hb7102849@gmail.com>
2024-02-06 10:29:17 +0530

    api

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,5 +1,5 @@
 
-const GOOGLE_API_KEY="AIzaSyCmwvtYdFaiG3e6uqK69TXNzPKuPRk7e0g";
+const GOOGLE_API_KEY="AIzaSyAs4qJKumqnviNB7OV_7xASYNX7T5ooN7I";
 
 export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
 
