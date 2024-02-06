7ba2bcfe8d310c079442779634b4bcb15f69d97f
Chandan <hb7102849@gmail.com>
2024-02-06 10:42:44 +0530

    env

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -3,7 +3,7 @@ const GOOGLE_API_KEY="AIzaSyAs4qJKumqnviNB7OV_7xASYNX7T5ooN7I";
 
 export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
 
-export const YOUTUBE_SUGGESTION_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_SUGGESTION_API="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const CommentData=[
     {
